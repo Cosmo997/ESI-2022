ab3ce56697fb9941a9b4a0cb4374d8ee5c3b2eb0
damianoserpetta <72809849+damianoserpetta@users.noreply.github.com>
2022-05-11 13:13:07 +0200

    openapi gen

--- build/main.js
+++ build/main.js
@@ -1,22 +1,26 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const camunda_config_1 = require("./config/camunda-config");
 const client_1 = require("./client");
+const openapi_1 = require("./api/src/generated-sources/openapi");
+const axios_1 = __importDefault(require("axios"));
 const clientManager = new client_1.ClientManager(camunda_config_1.baseUrl);
 const client = clientManager.getCLient();
-client.subscribe("creditScoreChecker", function ({ task, taskService }) {
-    return __awaiter(this, void 0, void 0, function* () {
-        // Put your business logic
-        // complete the task
-        yield taskService.complete(task);
-    });
+client.subscribe("creditScoreChecker", async function ({ task, taskService }) {
+    // Put your business logic
+    // complete the task
+    await taskService.complete(task);
+});
+const axiosInstance = axios_1.default.create({
+    baseURL: camunda_config_1.baseUrl,
+    timeout: 1000,
+});
+const apiConfig = new openapi_1.Configuration({});
+const engineApi = new openapi_1.EngineApi(apiConfig, camunda_config_1.baseUrl, axiosInstance);
+const res = engineApi.getProcessEngineNames();
+res.then(function (result) {
+    console.log(result);
 });
