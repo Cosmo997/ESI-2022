9c7434b41580d64482db95a6b1d5d7844a4487a4
damianoserpetta <72809849+damianoserpetta@users.noreply.github.com>
2022-05-11 13:13:07 +0200

    openapi gen

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { baseUrl } from "./config/camunda-config";
 import { ClientManager } from "./client";
+import {
+  EngineApi,
+  Configuration,
+  ConfigurationParameters,
+} from "./api/src/generated-sources/openapi";
+import axios, { AxiosInstance } from "axios";
 
 const clientManager = new ClientManager(baseUrl);
 
@@ -9,4 +15,18 @@ client.subscribe("creditScoreChecker", async function ({ task, taskService }) {
   // Put your business logic
   // complete the task
   await taskService.complete(task);
+});
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: baseUrl,
+  timeout: 1000,
+});
+
+const apiConfig = new Configuration({});
+
+const engineApi = new EngineApi(apiConfig, baseUrl, axiosInstance);
+const res = engineApi.getProcessEngineNames();
+
+res.then(function (result) {
+  console.log(result);
 });
