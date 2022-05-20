91332c0a54f9bd576e18618063cf72c6aaedf470
Moschini Daniele <daniele_moschini@hotmail.it>
2022-05-20 19:45:34 +0200

    [UPDATE] Start implementing subManager

--- src/Workers/HelpDesk/HelpDeskWorker.ts
+++ src/Workers/HelpDesk/HelpDeskWorker.ts
@@ -1,102 +1,117 @@
-import { ValueMap, Variables } from "camunda-external-task-client-js";
-import { CorrelationMessageDto, VariableValueDto } from "../../api/src/generated-sources/openapi";
+import { Client, ValueMap, Variables } from "camunda-external-task-client-js";
+import {
+  CorrelationMessageDto,
+  VariableValueDto,
+} from "../../api/src/generated-sources/openapi";
 import { ClientManager } from "../../client";
 import { baseUrl } from "../../config/camunda-config";
 import { titleCaseWord } from "../../Helpers/extension";
 import { MessageController } from "../../APIController/message_controller";
 import { v4 } from "uuid";
 
-
-export class HelpDeskWorker{
-    
-   public ticketSubscription(topic: string, messageName: string){
-        const client = new ClientManager(baseUrl).getClient();
-        client.subscribe(topic, async  ({ task, taskService }) => {
-            console.log(`\n\n------------ TICKET WORKER STARTED TASK ID: ${task.id}------------\n`);
-            const correlationMessageDto: CorrelationMessageDto = this._generateCorrelationMessageDTO(
-                messageName,
-                task.businessKey,
-                task.variables.getAll()
-            );
-            await this._sendMessage(correlationMessageDto);
-
-            const newProcessVariables = new Variables().set(
-                "ticket-opening-date",
-                new Date()
-            );
-            await taskService.complete(task, newProcessVariables);
-        
-            console.log(`\n------------TICKET WORKER TERMINATED TASK ID: ${task.id} ------------\n\n`);
-            client.stop();
-          });
-   }
-
-   public saveTicket(topic: string){
-    const client = new ClientManager(baseUrl).getClient();
-    client.subscribe(topic, async  ({ task, taskService }) => {
-        console.log(`\n\n------------ SAVE TICKET OPERATION STARTED \nTASK ID: ${task.id}------------\n`);
-        const newProcessVariables = new Variables().setAll(
-          {"ticket-save-date" : new Date(), "ticketId" : v4(), },
+export abstract class SubManager {
+  public subWithMessage(client: Client, topic: string, messageName: string) {
+    client.subscribe(topic, async ({ task, taskService }) => {
+      const correlationMessageDto: CorrelationMessageDto =
+        this.generateCorrelationMessageDTO(
+          messageName,
+          task.businessKey,
+          task.variables.getAll()
         );
+      await this.sendMessage(correlationMessageDto);
+      await taskService.complete(task);
+    });
+  }
 
-        // Put here save logic...
+  public subWithoutMessage(client: Client, topic: string) {
+    client.subscribe(topic, async ({ task, taskService }) => {
+      // do someting...
+      await taskService.complete(task);
+    });
+  }
 
-
-        await taskService.complete(task, newProcessVariables);
-    
-        console.log(`\n------------SAVE TICKET OPERATION TERMINATED \nTASK ID: ${task.id} ------------\n\n`);
-        client.stop();
-      });
-   }
-
-   public updateTicket(topic: string){
-    const client = new ClientManager(baseUrl).getClient();
-    client.subscribe(topic, async  ({ task, taskService }) => {
-        console.log(`\n\n------------ UPDATE TICKET OPERATION STARTED \nTASK ID: ${task.id}------------\n`);
-        const newProcessVariables = new Variables().setAll(
-          { "ticket-update-date": new Date(), "ticketStatus": "closed"}
-        );
-
-        // Put here update logic...
-
-        await taskService.complete(task, newProcessVariables);
-    
-        console.log(`\n------------UPDATE TICKET OPERATION TERMINATED \nTASK ID: ${task.id} ------------\n\n`);
-        client.stop();
-      });
-   }
-
-   private _generateCorrelationMessageDTO(messageName: string, businessKey:  string | undefined, variables: ValueMap): CorrelationMessageDto  {
-        const jsonObject = JSON.parse(JSON.stringify(variables))
-        let map = new Map<string, any>()  
-        for (var value in jsonObject) {  
-            map.set(value,jsonObject[value])  
-        }
-        this._printVariables(map);
-        let processVariables : { [key: string]: VariableValueDto} = {};
-        for (let [key, value] of map) {
-            processVariables[key] = {value: value, type: titleCaseWord(typeof value)};
-        }
-        const correlationMessageDto: CorrelationMessageDto = {
-            messageName: messageName,
-            businessKey: businessKey,
-            processVariables: processVariables,
-          };
-        return correlationMessageDto;
-   }
-
-   private async _sendMessage(correlationMessageDto: CorrelationMessageDto){
-        const messageController = new MessageController();
-        await messageController.sendMessage(correlationMessageDto);
-        console.log("\nMessage Sent!\n");
-   }
-
-   private _printVariables(map: Map<string, any>){
-    console.log("\nTASK VARIABLES: \n")
-    for (let [key, value] of map) {
-        if(value != undefined){
-            console.log(`${titleCaseWord(key)} :`, value);
-        }
+  protected generateCorrelationMessageDTO(
+    messageName: string,
+    businessKey: string | undefined,
+    variables: ValueMap
+  ): CorrelationMessageDto {
+    const jsonObject = JSON.parse(JSON.stringify(variables));
+    let map = new Map<string, any>();
+    for (var value in jsonObject) {
+      map.set(value, jsonObject[value]);
     }
-   }
+    this.printVariables(map);
+    let processVariables: { [key: string]: VariableValueDto } = {};
+    for (let [key, value] of map) {
+      processVariables[key] = {
+        value: value,
+        type: titleCaseWord(typeof value),
+      };
+    }
+    const correlationMessageDto: CorrelationMessageDto = {
+      messageName: messageName,
+      businessKey: businessKey,
+      processVariables: processVariables,
+    };
+    return correlationMessageDto;
+  }
+
+  protected async sendMessage(correlationMessageDto: CorrelationMessageDto) {
+    const messageController = new MessageController();
+    await messageController.sendMessage(correlationMessageDto);
+    console.log("\nMessage Sent!\n");
+  }
+
+  protected printVariables(map: Map<string, any>) {
+    console.log("\nTASK VARIABLES: \n");
+    for (let [key, value] of map) {
+      if (value != undefined) {
+        console.log(`${titleCaseWord(key)} :`, value);
+      }
+    }
+  }
+}
+
+export class HelpDeskWorker extends SubManager {
+  public openTicket(topic: string, messageName: string) {
+    const client = new ClientManager(baseUrl).getClient();
+    super.subWithMessage(client, topic, messageName);
+  }
+
+  public closeTicket(topic: string, messageName: string) {
+    const client = new ClientManager(baseUrl).getClient();
+    super.subWithMessage(client, topic, messageName);
+  }
+
+  public notifyTicket(topic: string, messageName: string) {
+    const client = new ClientManager(baseUrl).getClient();
+    super.subWithMessage(client, topic, messageName);
+  }
+
+  public saveTicket(topic: string) {
+    const client = new ClientManager(baseUrl).getClient();
+    client.subscribe(topic, async ({ task, taskService }) => {
+      console.log(
+        `\n\n------------ SAVE TICKET OPERATION STARTED \nTASK ID: ${task.id}------------\n`
+      );
+      const newProcessVariables = new Variables().setAll({
+        "ticket-save-date": new Date(),
+        ticketId: v4(),
+      });
+
+      // Put here save logic...
+
+      await taskService.complete(task, newProcessVariables);
+
+      console.log(
+        `\n------------SAVE TICKET OPERATION TERMINATED \nTASK ID: ${task.id} ------------\n\n`
+      );
+      client.stop();
+    });
+  }
+
+  public updateTicket(topic: string) {
+    const client = new ClientManager(baseUrl).getClient();
+    super.subWithoutMessage(client, topic);
+  }
 }
--- src/Workers/Suppliers/NewSupplier/NewSupplierWorker.ts
+++ src/Workers/Suppliers/NewSupplier/NewSupplierWorker.ts
@@ -7,23 +7,23 @@ async function main() {
   // subToOpenTicketForNewSupplier();
   const helpdesk = new HelpDeskWorker();
   
-  helpdesk.ticketSubscription('open-ticket-new-supplier','new-ticket-message-new-supplier');
+  helpdesk.openTicket('open-ticket-new-supplier','new-ticket-message-new-supplier');
   // Save ticket
   helpdesk.saveTicket('save-ticket-new-supplier');
   // Update ticket
   helpdesk.updateTicket('update-ticket-new-supplier');
 
   // Notify IT
-  helpdesk.ticketSubscription('notify-it-developer-new-supplier', "notify-it-new-supplier-message");
+  helpdesk.notifyTicket('notify-it-developer-new-supplier', "notify-it-new-supplier-message");
   
   // Close ticket
-  helpdesk.ticketSubscription('close-ticket-new-supplier', 'close-ticket-new-supplier');
+  helpdesk.closeTicket('close-ticket-new-supplier', 'close-ticket-new-supplier');
   
   // Notify
-  helpdesk.ticketSubscription('notify-admin-new-supplier','notify-admin-credential-new-supplier');
+  helpdesk.notifyTicket('notify-admin-new-supplier','notify-admin-credential-new-supplier');
 
   // Notify Ticket Owner
-  helpdesk.ticketSubscription('notify-owner-new-supplier','notify-ticket-owner-message-new-supplier');
+  helpdesk.notifyTicket('notify-owner-new-supplier','notify-ticket-owner-message-new-supplier');
 
   subToNotifySupplierCredentialForNewSupplier();
 
