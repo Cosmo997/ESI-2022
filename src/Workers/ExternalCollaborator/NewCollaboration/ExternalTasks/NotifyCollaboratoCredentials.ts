de06ecc497cff768b86c73bc344f24b20affdfa1
Moschini Daniele <daniele_moschini@hotmail.it>
2022-06-11 22:07:09 +0200

    [UPDATE] Added HTML form for NewCollaborator
    S'è rotto close ticket e non so perchè, ma le form funzionano.
    Non vanno i controlli sulla password

--- src/Workers/ExternalCollaborator/NewCollaboration/ExternalTasks/NotifyCollaboratoCredentials.ts
+++ src/Workers/ExternalCollaborator/NewCollaboration/ExternalTasks/NotifyCollaboratoCredentials.ts
@@ -8,8 +8,8 @@ export class NotifyCollaboratorCredentialsExternalTask
     console.log("\n\n------------ NOTIFY CREDENTIALS ------------\n");
 
 
-    console.log("USERNAME: " + task.variables.get("collab-user"));
-    console.log("PASSWORD: " + task.variables.get("collab-pass"));
+    console.log("USERNAME: " + task.variables.get("COLLAB_USERNAME"));
+    console.log("PASSWORD: " + task.variables.get("COLLAB_PASSWORD"));
     await taskService.complete(task);
 
     console.log("\n------------ NOTIFY CREDENTIALS TERMINATED------------\n\n");
